c6de11471ba7a4979170a008a5d426cc2bede18d
Daniel <mocaibre@gmail.com>
2025-06-30 21:13:45 -0300

    Configurando Prisma para  conectar no MYSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
+import { AuthModule } from './auth/auth.module';
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [AuthModule, UserModule, DatabaseModule],
 })
 export class AppModule {}
